ffde335d2188d0cb77667b97d7dab486ec216f48
shinhwagk <shanghai_sl@hotmail.com>
2019-12-05 16:26:14 +0800

    Update main_test.py

--- src/sqlexecuter/main_test.py
+++ src/sqlexecuter/main_test.py
@@ -2,6 +2,11 @@ import unittest
 import os
 from main import createDBcnx, executeDDLs
 
+"""
+1007 (HY000): Can't create database 'test1'; database exists
+1064 (42000): You have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the right syntax to use near 'database1 test1' at line 1
+"""
+
 
 class TestSum(unittest.TestCase):
 
@@ -9,7 +14,8 @@ class TestSum(unittest.TestCase):
         cnx = createDBcnx(os.environ["host"], os.environ["port"], os.environ["user"],
                           os.environ["password"])
         rs = executeDDLs(
-            cnx, ["create database test1", "create database test1"])
+            cnx, ["create database test1", "create database test2"])
+        print(rs[1])
         self.assertEqual(rs, ["ok", "ok"], "Should be 6")
 
 
