2893f07b3df63f4d0e05d9fbc8ed1b15ccb11e42
shinhwagk <shanghai_sl@hotmail.com>
2019-12-05 13:37:04 +0800

    update

--- src/sqlexecuter/main_test.py
+++ src/sqlexecuter/main_test.py
@@ -1,5 +1,5 @@
 import unittest
-import sys
+import os
 from main import createDbConn
 
 
@@ -7,12 +7,9 @@ class TestSum(unittest.TestCase):
 
     def test_sum(self):
         self.assertEqual(sum([1, 2, 3]), 6, "Should be 6")
-        self.assertEqual(createDbConn("127.0.0.1", command_line_param, "root",
+        self.assertEqual(createDbConn("127.0.0.1", os.environ["port"], "root",
                                       "password", ["create database abc"]), 1, "Should be 1")
 
 
 if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        sys.exit("ERRORommand-line parameter must be supplied for these tests")
-    command_line_param = sys.argv[1]
     unittest.main()
