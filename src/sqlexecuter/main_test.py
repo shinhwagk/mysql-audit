0256d732a0cca02728ad7dd7500160205a5878ef
shinhwagk <shanghai_sl@hotmail.com>
2019-12-05 11:17:28 +0800

    Update main_test.py

--- src/sqlexecuter/main_test.py
+++ src/sqlexecuter/main_test.py
@@ -7,7 +7,7 @@ class TestSum(unittest.TestCase):
     def test_sum(self):
         self.assertEqual(sum([1, 2, 3]), 6, "Should be 6")
         self.assertEqual(createDbConn("127.0.0.1", 3306, "root",
-                                      "123456", ["create database abc"]), 1, "Should be 1")
+                                      "password", ["create database abc"]), 1, "Should be 1")
 
 
 if __name__ == '__main__':
