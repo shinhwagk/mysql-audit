ad53711d4da0b0db58f53b9123062e1c63444187
shinhwagk <shanghai_sl@hotmail.com>
2019-12-05 16:21:39 +0800

    test

--- src/sqlexecuter/main_test.py
+++ src/sqlexecuter/main_test.py
@@ -8,8 +8,9 @@ class TestSum(unittest.TestCase):
     def test_sum(self):
         cnx = createDBcnx(os.environ["host"], os.environ["port"], os.environ["user"],
                           os.environ["password"])
-        executeDDLs(cnx, ["create database test1", "create database1 test2"])
-        self.assertEqual(sum([1, 2, 3]), 6, "Should be 6")
+        rs = executeDDLs(
+            cnx, ["create database test1", "create database test1"])
+        self.assertEqual(rs, ["ok", "ok"], "Should be 6")
 
 
 if __name__ == '__main__':
